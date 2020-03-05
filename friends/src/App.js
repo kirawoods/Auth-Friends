cd978bc91e0b8e85b2992363ac7c1d051937f864
Kira <kawoods826@gmail.com>
2020-03-05 10:40:14 -0800

    adds Login Page

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { Login } from "./components/Login";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello Friends</div>;
+  return (
+    <Router>
+      <div className="App">
+        <div className="Navigation">
+          <Login />
+        </div>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ friends/src/components/Login.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+
+export class Login extends Component {
+  state = {
+    credentials: {
+      username: "",
+      password: ""
+    }
+  };
+
+  handleChange = e => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  login = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("/login", this.state.credentials)
+      .then(res => {
+        localStorage.setItem("token", res.data.payload);
+        this.props.history.push("/protected");
+      })
+      .catch(error => console.log(error));
+  };
+
+  render() {
+    return (
+      <div className="login-form">
+        <form onSubmit={this.login}>
+          <label>Username</label>
+          <input
+            type="text"
+            name="username"
+            value={this.state.credentials.username}
+            onChange={this.handleChange}
+          />
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            value={this.state.credentials.password}
+            onChange={this.handleChange}
+          />
+          <button>Log In</button>
+        </form>
+      </div>
+    );
+  }
+}
