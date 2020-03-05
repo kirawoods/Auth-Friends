90e6b82aeaa9ce2d2f77078900aca4ddda25b16d
Kira <kawoods826@gmail.com>
2020-03-05 10:57:29 -0800

    added Private Route

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import { Login } from "./components/Login";
+import { PrivateRoute } from "./components/PrivateRoute";
+import { Friends } from "./components/Friends";
 import "./App.css";
 
 function App() {
@@ -9,9 +11,12 @@ function App() {
       <div className="App">
         <div className="Navigation">
           <Link to="/login">Login</Link>
+          <Link to="/protected">Friends List</Link>
         </div>
         <Switch>
+          <PrivateRoute exact path="/protected" component={Friends} />
           <Route path="/login" component={Login} />
+          <Route component={Login} />
         </Switch>
       </div>
     </Router>
