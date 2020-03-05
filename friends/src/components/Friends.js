4d4b24c5c0ce2643f6e43b74e528210db2f47735
Kira <kawoods826@gmail.com>
2020-03-05 11:04:32 -0800

    added friends list component

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 export const Friends = () => {
-  return <div>Friends</div>;
+  const [friends, setFriends] = useState([]);
+
+  const getFriends = () => {
+    axiosWithAuth()
+      .get("/friends")
+      .then(res => {
+        setFriends(res.data);
+      })
+      .catch(err => console.log(err.response));
+  };
+
+  useEffect(() => {
+    getFriends();
+  }, []);
+
+  return (
+    <div>
+      <h2>My Friends List</h2>
+      {friends.map(friend => (
+        <div className="friend-card" key={friend.id}>
+          <h3>{friend.name}</h3>
+          <h4>Age: {friend.age}</h4>
+          <h5>Email: {friend.email}</h5>
+        </div>
+      ))}
+    </div>
+  );
 };
